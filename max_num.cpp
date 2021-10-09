99ea7409c66de9eeca9104de2e8879b41712e543
Atlas <32842649+Adityamirpagar@users.noreply.github.com>
2021-10-09 12:14:03 +0530

    Add files via upload

---
+++ max_num.cpp
@@ -0,0 +1,32 @@
+// write a program to check the bigger number, with 5 inputs.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int num_1, num_2, num_3, num_4, num_5;
+
+    cout << " Enter the number 1: ";cin >> num_1 ; 
+    cout << " Enter the number 2: ";cin >> num_2 ;
+    cout << " Enter the number 3: ";cin >> num_3 ;
+    cout << " Enter the number 4: ";cin >> num_4 ;
+    cout << " Enter the number 5: ";cin >> num_5 ;
+
+    int max_num = num_1;
+
+    if(num_2 > max_num) 
+        max_num = num_2 ;
+    if(num_3 > max_num) 
+        max_num = num_3 ;
+    if(num_4 > max_num) 
+        max_num = num_4 ;
+    if(num_5 > max_num) 
+        max_num = num_5 ;
+
+    cout << " max_num is : " << max_num << endl;
+
+
+    return 0;
+}
