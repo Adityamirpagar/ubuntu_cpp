01b08b4a5fcfa1360bca1152e015ce888b17d435
Atlas <32842649+Adityamirpagar@users.noreply.github.com>
2021-10-11 10:49:02 +0530

    Create infinite_for.cpp

---
+++ loop/for_loop/infinite_for.cpp
@@ -0,0 +1,15 @@
+/*
+This program will run infinitely, because as in maths "where there is no value there's 1 (TRUE)."
+So the for loop will always stay true, the program will run infinitely.
+*/
+
+#include <iostream>
+
+int main() {
+  
+  for( ; ; ){
+    
+    std::cout << "infinite" << std::endl;
+  }
+
+}
