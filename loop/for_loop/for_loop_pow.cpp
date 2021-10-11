0c81f4476cdc6f18da7c55db0aed60c2441db96d
Atlas <32842649+Adityamirpagar@users.noreply.github.com>
2021-10-11 10:49:23 +0530

    Add files via upload

---
+++ loop/for_loop/for_loop_pow.cpp
@@ -0,0 +1,14 @@
+// This program gives the value of the ecponents of 2.
+
+#include <iostream>
+
+int main() {
+int pow = 1;
+    for(int i =0; i < 17 ; i ++){
+
+        std::cout << "2 to the power of " << i << " is " << pow << std::endl;
+        
+        pow *= 2;
+    }
+
+}
